ebab68ff75d935e309decd81d8f8e474998b0cdd
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2023-04-17 12:15:37 +0530

    Update font, nav, layout

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import "./globals.css"
+import Nav from "./auth/Nav"
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body
+        className={`mx-4 md:mx-48 xl:mx-96 ${poppins.variable} font-sans bg-gray-200`}
+      >
+        <Nav />
+        {children}
+      </body>
+    </html>
+  )
+}
