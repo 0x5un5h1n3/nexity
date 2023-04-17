19331ad3f90106d95195ce9a8671c2673f04423a
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2023-04-17 12:16:24 +0530

    Update, font, nav, layout

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default async function Nav() {
+
+  return (
+    <nav className="flex justify-between items-center py-8 ">
+      <Link href={"/"}>
+        <h1 className="font-bold text-lg">Nexity</h1>
+      </Link>
+    </nav>
+  )
+}
