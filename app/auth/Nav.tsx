7089ab820c58fd55b757d7150a918d2dbccf84fa
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2023-04-17 12:39:04 +0530

    Add session logout

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link"
+import Login from "./Login"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../pages/api/auth/[...nextauth]"
 
 export default async function Nav() {
+
+  const session = await getServerSession(authOptions)
+  console.log(await getServerSession(authOptions))
 
   return (
     <nav className="flex justify-between items-center py-8 ">
       <Link href={"/"}>
         <h1 className="font-bold text-lg">Nexity</h1>
       </Link>
+      <ul className="flex items-center gap-6"></ul>
+      {!session?.user && <Login />}
+      {session?.user && <h1>{session?.user?.name}</h1>}
     </nav>
   )
 }
