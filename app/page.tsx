cd8048071223613333594166473aad7266cb0568
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2023-04-27 16:43:43 +0530

    Fetch all posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
+"use client"
+
+import Post from "./components/Post"
 import AddPost from "./components/AddPost"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+//Fetch All posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  })
+  if (error) return error
+  if (isLoading) return "Loading....."
+
   return (
-    <main>
-      <h1>Hello Next</h1>
+    <div>
       <AddPost />
-    </main>
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          comments={post.comments}
+        />
+      ))}
+    </div>
   )
 }
